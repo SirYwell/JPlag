21afd0ed0bdd5d4f734b96dc81b3a8e3b39f7585
SirYwell <hannesgreule@outlook.de>
2023-02-16 22:09:19 +0100

    Improve extractor

--- languages/java-configurable/src/main/java/de/jplag/java/configurable/util/TreeKindExtractor.java
+++ languages/java-configurable/src/main/java/de/jplag/java/configurable/util/TreeKindExtractor.java
@@ -2,6 +2,7 @@ package de.jplag.java.configurable.util;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -14,8 +15,13 @@ public class TreeKindExtractor {
                 .collect(Collectors.groupingBy(Tree.Kind::asInterface));
         StringBuilder builder = new StringBuilder();
         for (var entry : categories.entrySet()) {
+            String categoryName = toCategoryName(entry.getKey());
             builder.append("- ")
-                    .append(entry.getKey().getSimpleName().replace("Tree", ""));
+                    .append(categoryName);
+            if (entry.getValue().size() == 1 && entry.getValue().get(0).name().equals(categoryName)) {
+                builder.append(System.lineSeparator());
+                continue;
+            }
             for (Tree.Kind kind : entry.getValue()) {
                 builder.append(System.lineSeparator())
                         .append("  - ")
@@ -24,5 +30,10 @@ public class TreeKindExtractor {
             builder.append(System.lineSeparator());
         }
         System.out.println(builder);
+    }
+
+    private static String toCategoryName(Class<? extends Tree> clazz) {
+        String name = clazz.getSimpleName().replace("Tree", "");
+        return name.replaceAll("([a-z0-9])([A-Z])", "$1_$2").toUpperCase(Locale.ROOT);
     }
 }
